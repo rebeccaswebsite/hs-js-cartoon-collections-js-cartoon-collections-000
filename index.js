bd5914ccaa378e87a6371518185e094bb3521305
Rebecca Huseyin <rebecca_huseyin@outlook.com>
2019-01-29 15:52:59 +0000

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
 function dwarfRollCall(dwarves) {
+  return `1. ${dwarves[0]} 2. ${dwarves[1]} 3. ${dwarves[2]} `;
 }
 
-function summonCaptainPlanet(planeteerCalls){
+function summonCaptainPlanet(fruits){
+  for(var i=0;i<fruits.length;i++){
+    fruits[i]= fruits[i].toUpperCase() + "!";
+  }
+  return fruits;
 }
 
-function longPlaneteerCalls(words) {
+function longPlaneteerCalls(longCalls){
+  for(var i=0;i<longCalls[i].length;i++){
+    if (longCalls[i].length > 4)
+    return true;
+    else 
+    return false;
+  }
 }
 
-function findTheCheese (foods) {
+function findTheCheese (cheddarCheese) {
+  if (cheddarCheese.includes("cheddar", 0))
+  return "cheddar";
+  else if(cheddarCheese.includes("gouda", 0))
+  return"gouda";
+  else if(cheddarCheese.includes("camembert", 0))
+  return "camembert";
+  else 
+  return("no cheese!");
 }
